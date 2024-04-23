494df28a311d0673fd165c3d773bd71976a5379e
Alessandro Ricci <alessandro.ricci38@studio.unibo.it>
2024-04-23 16:33:21 +0200

    new implementation

--- app/src/main/java/com/example/musicevents/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/musicevents/ui/screens/home/HomeScreen.kt
@@ -2,14 +2,19 @@ package com.example.musicevents.ui.screens.home
 
 import android.graphics.drawable.Icon
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -19,14 +24,22 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import com.example.musicevents.R
 import com.example.musicevents.data.database.Event
 
 @Composable
@@ -43,6 +56,8 @@ fun HomeScreen(){
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EventItem(item: Event) {
+    var eventSaved by remember { mutableStateOf(false) }
+    val icon = if(eventSaved) Icons.Default.Favorite else Icons.Default.FavoriteBorder
     OutlinedCard(
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surface,
@@ -61,18 +76,28 @@ fun EventItem(item: Event) {
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold
             )
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = {
+                eventSaved = !eventSaved
+            }) {
                 Icon(
-                    Icons.Default.FavoriteBorder,
+                    icon,
                     contentDescription = "",
                     tint = Color.Black
                 )
             }
         }
+        AsyncImage(
+            model = "https://www.jambase.com/wp-content/uploads/2017/02/bring-me-the-horizon-bring-me-the-horizon-9f7d1441-dc68-4499-8c27-e14216d3cf24_220241_RETINA_LANDSCAPE_16_9-1480x832.jpg",
+            contentDescription = "The delasign logo",
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+                .border(BorderStroke(3.dp, Color.Black))
+        )
         Text(
             text = "In ${item.venue} on ${item.date}",
             modifier = Modifier
-                .padding(horizontal = 20.dp),
+                .padding(20.dp),
             fontSize = 15.sp
         )
     }
